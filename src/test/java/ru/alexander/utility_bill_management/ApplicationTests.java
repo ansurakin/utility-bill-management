d032633c8937cf964841fd77bfad1d935ad5a07c
alex <alex@example.ru>
2026-03-08 20:18:02 +0300

    add spring boot

---
+++ src/test/java/ru/alexander/utility_bill_management/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexander.utility_bill_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
